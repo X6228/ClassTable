6d8e6b01ad29cbeb86de157444abf8503bdb963f
X6228 <xh6520@qq.com>
2019-02-27 16:46:17 +0800

    2019-02-27 in o

--- SData.py
+++ SData.py
@@ -1,6 +1,11 @@
 class School(object):
-    def __init__(self):
-        self.name = ""
+    def __init__(self,name="宇宙无敌最大的小学"):
+        self.name = name
+        self.terchers = None
+        self.classrooms = None
+
+    def __str__(self):
+        return self.name
 
 class Teacher(object):
     def __init__(self):
@@ -10,14 +15,24 @@ class Teacher(object):
         self.jobs = []
 
 class ClassRoom(object):
-    def __init__(self):
-        self.name = ""
+    def __init__(self,grade=0,number=0):
+        self.name = "{0}年级{1}班".format(grade,number)
         self.grade = 0
         self.number = 0
         self.ctable = None
 
-class ClassTable(object):
-    pass
+    def __str__(self):
+        return self.name
 
-class Lessons(object):
-    pass
+class ClassTable(object):
+    def __init__(self):
+        self.owner = None
+
+class Lesson(object):
+    def __init__(self,name=""):
+        self.name = name
+        self.teacher = None
+        self.classroom = None
+
+    def __str__(self):
+        return self.name  
---
+++ test.py
@@ -0,0 +1,7 @@
+import SData
+l = SData.Lesson("数学")
+s = SData.School()
+c = SData.ClassRoom(4,1)
+print(s)
+print(c)
+print(l)
